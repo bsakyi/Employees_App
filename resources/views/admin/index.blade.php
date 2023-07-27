a99377cd9fc29430b8c585afaf966ba16be4c408
unknown <donasare@live.com>
2023-07-27 22:11:16 +0000

    Initial-Admin Layout Models and Empty Controllers

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+<x-admin-layout>
+
+	<h4>Welcome to Admin Page</h4>
+
+</x-admin-layout>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,15 @@
+<div class="min-h-screen bg-gray-100">
+    @include('layouts.admin-navigation')
+
+   <div class="flex space-x-4">
+    
+         <sidebar />
+
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main> 
+
+   </div>
+
+</div>
